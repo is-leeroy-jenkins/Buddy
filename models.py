e5f05abf74719759196486d3c425f2b3607bf48a
is-leeroy-jenkins <17190650+is-leeroy-jenkins@users.noreply.github.com>
2026-02-23 17:34:43 -0500

    update

--- models.py
+++ models.py
@@ -94,9 +94,8 @@ class Prompt( ):
 	id: Optional[ str ]
 	version: Optional[ str ]
 	
-	def __init__( self, instructions: str=None, content: str=None, variables: List[ str ]=None,
-			data: Dict[ str, Any ]=None, id: int=None, version: str=None ):
-		self.instructions = instructions
+	def __init__( self ):
+		self.instructions= None
 		self.content = content
 		self.variables = variables
 		self.data = data
@@ -163,10 +162,10 @@ class Message( ):
 	role: Optional[ str ]
 	type: Optional[ str ]
 	
-	def __init__( self, role: str=None, content: str=None,  type: str=None  ):
-		self.role = role
-		self.content = content
-		self.type = type
+	def __init__( self ):
+		self.role = None
+		self.content = None
+		self.type = None
 	
 	def __dir__( self ) -> List[ str ]:
 		'''
@@ -192,7 +191,7 @@ class Message( ):
 		'''
 		return self.content
 	
-class Payload( ):
+class AiConfig( ):
 	'''
 
 		Purpose:
@@ -256,8 +255,6 @@ class Payload( ):
 		The stop sequence will not be included as part of the response.
 		
 	'''
-	api_key: Optional[ str ]
-	model: Optional[ str ]
 	temperature: Optional[ float ]
 	top_percent: Optional[ float ]
 	max_tokens: Optional[ int ]
@@ -271,8 +268,6 @@ class Payload( ):
 	number: Optional[ int ]
 	
 	def __init__( self  ):
-		self.api_key = None
-		self.model = None
 		self.temperature = None
 		self.top_percent = None
 		self.presense = None
@@ -300,7 +295,7 @@ class Payload( ):
 		return [ 'temperature', 'top_percent', 'presense', 'store', 'stream', 'stop_sequence',
 		         'response_format', 'number', 'max_tokens', 'asynchronous' ]
 
-class GemmaConfig( Payload ):
+class GemConfig( AiConfig ):
 	'''
 		
 		Purpose:
@@ -310,24 +305,14 @@ class GemmaConfig( Payload ):
 		----------
 		
 	'''
+	model: Optional[ str ]
 	instructions: Optional[ str ]
 	
 	def __init__( self ):
-		api_key = cfg.GEMINI_API_KEY
-		model = None
-		self.temperature = None
-		self.top_percent = None
-		self.presense = None
-		self.store = None
-		self.stream = None
-		self.fequency_penalty = None
-		self.stop_sequence = None
-		self.response_format = None
-		self.number = None
-		self.max_tokens = None
-		self.background = None
+		self.model = None
+		self.instructions = None
 
-class GrokConfig( Payload ):
+class GrokConfig( ContentConfig ):
 	'''
 		
 		Purpose:
@@ -337,24 +322,14 @@ class GrokConfig( Payload ):
 		----------
 		
 	'''
+	model: Optional[ str ]
 	instructions: Optional[ str ]
 	
 	def __init__( self ):
-		self.api_key = cfg.GROQ_API_KEY
 		sself.model = None
-		self.temperature = None
-		self.top_percent = None
-		self.presense = None
-		self.store = None
-		self.stream = None
-		self.fequency_penalty = None
-		self.stop_sequence = None
-		self.response_format = None
-		self.number = None
-		self.max_tokens = None
-		self.background = None
+		self.instructions = None
 
-class GptConfig( Payload ):
+class GptConfig( AiConfig ):
 	'''
 		
 		Purpose:
@@ -364,13 +339,41 @@ class GptConfig( Payload ):
 		----------
 		
 	'''
+	model: Optional[ str ]
 	instructions: Optional[ str ]
 	
 	def __init__( self ):
-		api_key = cfg.OPENAI_API_KEY
-		model = None
+		self.model = None
+		self.instructions = None
 
-class TextConfig( Payload ):
+class ToolConfig( AiConfig ):
+	'''
+		
+		Purpose:
+		--------
+		
+		
+		Attributes:
+		----------
+		
+		
+	'''
+	domains: Optional[ List[ str ] ]
+	include: Optional[ List[ str ] ]
+	tools: Optional[ List[ Dict[ str, str ] ] ]
+	allow_parallel: Optional[ bool ]
+	tool_choice: Optional[ bool ]
+	max_tools: Optional[ int ]
+	
+	def __init__( self ):
+		self.domains = [ ]
+		self.tools = [ ]
+		self.include = [ ]
+		self.allow_parallel = None
+		self.tool_choice = None
+		self.max_tools = None
+		
+class TextConfig( AiConfig ):
 	'''
 
 		Purpose:
@@ -434,13 +437,7 @@ class TextConfig( Payload ):
 		The stop sequence will not be included as part of the response.
 		
 	'''
-	include: Optional[ List[ str ] ]
 	reasoning: Optional[ Dict[ str, str ] ]
-	domains: Optional[ List[ str ] ]
-	tools: Optional[ List[ Dict[ str, str ] ] ]
-	allow_parallel: Optional[ bool ]
-	tool_choice: Optional[ bool ]
-	max_tools: Optional[ int ]
 	file_path: Optional[ str ]
 	previous_id: Optional[ str ]
 	input: Optional[ List[ Dict[ str, str ] ] ] | str
@@ -449,21 +446,10 @@ class TextConfig( Payload ):
 	
 	def __init__( self ):
 		super( ).__init__(  )
-		self.response_format = None
-		self.model = None
-		self.allow_parallel = None
 		self.previous_id = None
-		self.include = [ ]
 		self.reasoning = { }
-		self.domains = [ ]
-		self.tools = [ ]
-		self.background = None
 		self.input = None
-		self.messages = [ ]
 		self.content = None
-		self.max_tools = None
-		self.file_path = None
-		self.tool_choice = None
 		
 	def __dir__( self ) -> List[ str ]:
 		'''
@@ -481,7 +467,7 @@ class TextConfig( Payload ):
 		         'response_format', 'number', 'max_tokens', 'background', 'include', 'reasoning',
 		         'domains', 'tools', 'allow_parallel', 'max_tools', 'messages', 'input', 'previous_id' ]
 
-class ImageConfig( Payload ):
+class ImageConfig( ToolConfig ):
 	'''
 
 		Purpose:
@@ -546,15 +532,10 @@ class ImageConfig( Payload ):
 		
 	'''
 	previous_id: Optional[ str ]
-	include: Optional[ List[ str ] ]
 	reasoning: Optional[ Dict[ str, str ] ]
-	domains: Optional[ List[ str ] ]
-	tools: Optional[ List[ Dict[ str, str ] ] ]
-	allow_parallel: Optional[ bool ]
-	tool_choice: Optional[ bool ]
-	max_tools: Optional[ int ]
 	image_path: Optional[ str ]
 	image_url: Optional[ str ]
+	image_data: Optional[ bytes ]
 	content: Optional[ List[ Dict[ str, str ] ] ] | str
 	size: Optional[ str ]
 	detail: Optional[ str ]
@@ -565,14 +546,7 @@ class ImageConfig( Payload ):
 	
 	def __init__( self ):
 		super( ).__init__(  )
-		self.include = [ ]
-		self.reasoning = { }
-		self.domains = [ ]
-		self.tools = [ ]
-		self.allow_parallel = None
-		self.max_tools = None
 		self.instructions = None
-		self.tool_choice = None
 		self.image_path = None
 		self.image_url = None
 		self.size = None
@@ -602,7 +576,7 @@ class ImageConfig( Payload ):
 		         'image_url', 'messages', 'size', 'detail', 'output_format', 'quality', 'reasoning',
 		         'style', 'backcolor', 'output_format', 'instructions', 'previous_id' ]
 
-class SpeechConfig( Payload ):
+class SpeechConfig( AiConfig ):
 	'''
 
 		Purpose:
@@ -691,96 +665,7 @@ class SpeechConfig( Payload ):
 		return [ 'temperature', 'top_percent', 'presense', 'store', 'stream', 'stop_sequence', 'model',
 		         'response_format', 'number', 'max_tokens', 'background', 'voice', 'sample_rate' ]
 	
-class TranscriptConfig( Payload ):
-	'''
-
-		Purpose:
-		--------
-		Represents a general-purpose request payload. This model intentionally
-		supports both request parameters (as echoed back) and response payload f
-		ields, while remaining permissive to vendor
-		evolution via `extra='ignore'`.
-
-		Attributes:
-		----------
-		response_format: Optional[ ResponseFormat ]
-		The response format settings used or returned by the upstream system.
-
-		temperature: Optional[ float ]
-		Optional. Controls the randomness of the output.
-		Values can range from [0.0, 2.0].
-
-		top_p: Optional[ float ]
-		Optional. The maximum cumulative probability of tokens to consider when sampling.
-		The model uses combined Top-k and Top-p (nucleus) sampling. Tokens are sorted based on
-		their assigned probabilities so that only the most likely tokens are considered.
-		Top-k sampling directly limits the maximum number of tokens to consider,
-		while Nucleus sampling limits the number of tokens based on the cumulative probability.
-
-		max_output_tokens: Optional[ int ]
-		Max output tokens cap.
-
-		store: Optional[ bool ]
-		Whether the upstream should store the response (provider-specific).
-
-		stream: Optional[ bool ]
-		Whether streaming was requested.
-		
-		top_k: Optional[ int ]
-		Optional. The maximum number of tokens to consider when sampling.
-		Gemini models use Top-p (nucleus) sampling or a combination of Top-k and nucleus sampling.
-		Top-k sampling considers the set of topK most probable tokens. Models running with nucleus
-		sampling don't allow topK setting.
-		
-		presence_penalty: Optional[ float ]
-		Optional. Presence penalty applied to the next token's logprobs if the token has already
-		been seen in the response. This penalty is binary on/off and not dependant on the
-		number of times the token is used (after the first).
-		
-		frequency_penalty: Optional[float ]
-		Optional. Frequency penalty applied to the next token's logprobs, multiplied by the number
-		of times each token has been seen in the respponse so far. A positive penalty will
-		discourage the use of tokens that have already been used, proportional to the number of
-		times the token has been used: The more a token is used, the more difficult it is for
-		the model to use that token again increasing the vocabulary of responses.
-		
-		logprobs: Optional[ int ]
-		Optional. Only valid if responseLogprobs=True. This sets the number of top logprobs to
-		return at each decoding step in the Candidate.logprobs_result.
-		The number must be in the range of [0, 20].
-		
-		stops: Optional[ List[str] ]
-		Optional. The set of character sequences (up to 5) that will stop output generation.
-		If specified, the API will stop at the first appearance of a stop_sequence.
-		The stop sequence will not be included as part of the response.
-		
-	'''
-	sample_rate: Optional[ int ]
-	language: Optional[ str ]
-	
-	def __init__( self ):
-		super( ).__init__( )
-		self.sample_rate = None
-		self.model = None
-		self.language = None
-	
-	def __dir__( self ) -> List[ str ]:
-		'''
-			
-			Purpose:
-			--------
-			Provides a list of public members of the class.
-			
-			Returns:
-			--------
-			List[ str ]
-		
-		'''
-		return [ 'temperature', 'top_percent', 'presense', 'store', 'stream', 'stop_sequence',
-		         'model', 'response_format', 'number', 'max_tokens', 'background',
-		         'language', 'sample_rate' ]
-
-class TranslateConfig( Payload ):
+class AudioConfig( AiConfig ):
 	'''
 
 		Purpose:
@@ -847,8 +732,7 @@ class TranslateConfig( Payload ):
 	output_format: Optional[ str ]
 	sample_rate: Optional[ int ]
 	voice: Optional[ str ]
-	source_language: Optional[ str ]
-	target_language: Optional[ str ]
+	language: Optional[ str ]
 	
 	def __init__( self ):
 		super( ).__init__( )
