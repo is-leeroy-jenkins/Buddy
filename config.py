9becfc21e0611a81f248d5a7beba02c70ecccf65
is-leeroy-jenkins <terry.eppler@gmail.com>
2026-04-01 20:31:24 -0400

    update

--- config.py
+++ config.py
@@ -43,6 +43,7 @@
   '''
 import os
 import re
+import multiprocessing
 
 
 #------------- COMMON CONSTANTS ---------------------
@@ -69,6 +70,8 @@ PROVIDERS = { 'GPT': 'gpt', 'Gemini': 'gemini', 'Grok': 'grok', }
 PROMPT_ID = 'pmpt_697f53f7ddc881938d81f9b9d18d6136054cd88c36f94549'
 PROMPT_VERSION = '16'
 LOCAL_AUDIO_PATH = r'stores/audio/conditions.mp3'
+DEFAULT_CTX = 4096
+CORES = multiprocessing.cpu_count( )
 
 # -------------- API KEYS ---------------------
 OPENAI_API_KEY = os.getenv( 'OPENAI_API_KEY' )
